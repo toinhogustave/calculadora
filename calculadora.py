dbf8a66a3c88d77b84c1d5fdcd6711b54f1725a7
toinhogustave <toinhogustave@gmail.com>
2026-06-12 11:13:52 -0300

    Adiciona calculadora

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+opcao=int(input("=========calculadora=========\n1.soma\n2.subtracao\n3.multiplicaçao\n4.divisão\n5.Fluxo de Caixa\n6.Cálculo de posição de queda de um projétil\nEscolha uma das opções:"))
+
+if opcao==1:
+    soma(a,b)
+elif opçao==2:
+    subtraçao(a,b)
+elif opcao==3:
+    multiplicaçao(a,b)
+elif opcao==4:
+    divisao(a,b)
+elif opcao==5:
+    fluxo(a,b)
+elif opcao==6:
+    queda(a,b)
