13e186c400939b00306018a70fd57b1182801a9d
toinhogustave <toinhogustave@gmail.com>
2026-06-12 22:13:01 -0300

    multiplicacao

--- calculadora.py
+++ calculadora.py
@@ -4,13 +4,14 @@ if opcao==1:
     a=int(input('digite um valor:'))
     b=int(input('outro valor:'))
     print(f'a soma é:{a+b}')
-elif opçao==2:
+elif opcao==2:
     a=int(input('digite um valor:'))
     b=int(input('outro valor:'))
     print(f'a subtração é:{a-b}')
-    subtraçao(a,b)
 elif opcao==3:
-    multiplicaçao(a,b)
+    a=int(input('digite um valor:'))
+    b=int(input('outro valor:'))
+    print(f'a multiplicação é:{a*b}')
 elif opcao==4:
     divisao(a,b)
 elif opcao==5:
