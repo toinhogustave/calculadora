b59aa9cfc32417cd88acc275f2d144ae4c8b9cc3
toinhogustave <toinhogustave@gmail.com>
2026-06-12 22:05:12 -0300

    soma

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,9 @@
 opcao=int(input("=========calculadora=========\n1.soma\n2.subtracao\n3.multiplicaçao\n4.divisão\n5.Fluxo de Caixa\n6.Cálculo de posição de queda de um projétil\nEscolha uma das opções:"))
 
 if opcao==1:
-    soma(a,b)
+    a=int(input('digite um valor:'))
+    b=int(input('outro valor:'))
+    print(f'a soma é:{a+b}')
 elif opçao==2:
     subtraçao(a,b)
 elif opcao==3:
